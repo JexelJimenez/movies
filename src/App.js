4e979a08d421d56ec82b39c5bda3a76c3813c7ee
jerick jimenez <jerick1023.jimenez@gmail.com>
2023-05-10 15:07:03 +0800

    listing of movies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
+import { 
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  Grid
+} from '@mui/material';
+// import ExpandMore from '@mui/icons-material/ExpandMore';
+
 
 function App() {
+
+  const [movies,setMovies] = useState(null)
+
+  useEffect(()=> {
+    axios.get('https://api.themoviedb.org/3/discover/movie?api_key=e3a89ca39100bceea4b71882246c87ff&language=en-US&sort_by=popularity.desc&page=1')
+    .then(res => {
+      console.log(res.data.results)
+      setMovies(res.data.results)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  if(!movies){
+    return null
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {movies.map((movie,i) => (
+        <Accordion key={movie.id}>
+          <AccordionSummary
+            // expandIcon={<ExpandMore/>}
+            arial-control='panel1a-content'
+            id='panel1a-content'
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={8} justifyContent="flex-start">
+                <Typography>{movie.title}</Typography>
+              </Grid>
+              <Grid item xs={4} justifyContent="flex-end">
+                <Typography>{movie.vote_average}/10</Typography>
+              </Grid>
+            </Grid>
+          </AccordionSummary>
+          <AccordionDetails>
+            {movie.overview}
+          </AccordionDetails>
+        </Accordion>
+        // <li key={movie.id} >{movie.title}</li>
+      ))}
+      
     </div>
   );
 }
